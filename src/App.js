9cb0cd50146d126f5e7e73b297d50000b091d99c
mayuxian <mayuxian@agree.com.cn>
2019-10-07 17:10:33 +0800

    调整首页为购物页面

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
+import CartPage from './components/Shop/CartPage';
 
 export default class App extends Component {
   render() {
     return (
-      <div >
-        哈哈哈！
-        <button>点击我！</button>
-      </div>
+      <CartPage></CartPage>
     );
   }
 }
--- src/components/Shop/GoodStore.js
+++ src/components/Shop/GoodStore.js
@@ -2,9 +2,9 @@ const GoodStore = {
   getGoods() {
     //正常应该请求数据，目前直接返回模拟数据
     return [
-      { id: "1", name: "苹果", price=5.0 },
-      { id: "2", name: "香蕉", price=6.0 },
-      { id: "3", name: "橘子", price=3.0 },
+      { id: "1", name: "苹果", price: 5.0 },
+      { id: "2", name: "香蕉", price: 6.0 },
+      { id: "3", name: "橘子", price: 3.0 },
     ]
   }
 }
